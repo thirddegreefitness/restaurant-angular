1534479067b8fcaac00a9594cb88e394bbc9074d
Drew Templeton <drew@drewtempleton.com>
2014-09-26 12:06:10 -0500

    boilerplate json api

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -11,16 +11,11 @@ angular
         'ui.sortable',
         'LocalStorageModule'
     ])
-    .config(['localStorageServiceProvider',
-        function(localStorageServiceProvider) {
-            localStorageServiceProvider.setPrefix('ls');
-        }
-    ])
     .config(function($routeProvider) {
         $routeProvider
             .when('/', {
                 templateUrl: 'views/main.html',
-                controller: 'MainCtrl'
+                controller: 'FetchController'
             })
             .when('/about', {
                 templateUrl: 'views/about.html',
@@ -30,3 +25,29 @@ angular
                 redirectTo: '/'
             });
     });
+
+// var mytodoApp = angular.module('mytodoApp')
+
+// function randomString(length, chars) {
+//     var result = '';
+//     for (var i = length; i > 0; --i) result += chars[Math.round(Math.random() * (chars.length - 1))];
+//     return result;
+// }
+
+// mytodoApp.factory("mytodoApp", function($http) {
+//     return {
+//         "retrieveYelp": function(name, callback) {
+//             $http.jsonp("http://api.yelp.com/v2/search?term=food&location=San+Francisco&callback=JSON_CALLBACK", {
+//                 params: {
+//                     oauth_consumer_key: "V-fz717e6id9rjQojd65HQ",
+//                     oauth_token: "6oYKZgIYiMXKSgofWWYJDuhiukvX3JVk",
+//                     oauth_signature_method: "hmac-sha1",
+//                     oauth_signature: "xZCZ4a3vcp02VTvHlVXw5Kkb__k",
+//                     oauth_timestamp: new Date().getTime(),
+//                     oauth_nonce: randomString(32, '0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ')
+//                 }
+//             }).success(callback);
+
+//         }
+//     }
+// });
