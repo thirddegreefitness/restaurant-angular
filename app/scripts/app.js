1523878f7652eda1236d9b398e93a713ca1dc18c
Drew Templeton <drew@drewtempleton.com>
2014-09-26 12:25:50 -0500

    removed extra stuff to make a clean starter app

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -1,7 +1,7 @@
 'use strict';
 
 angular
-    .module('mytodoApp', [
+    .module('restaurantApp', [
         'ngAnimate',
         'ngCookies',
         'ngResource',
@@ -15,39 +15,9 @@ angular
         $routeProvider
             .when('/', {
                 templateUrl: 'views/main.html',
-                controller: 'FetchController'
-            })
-            .when('/about', {
-                templateUrl: 'views/about.html',
-                controller: 'AboutCtrl'
+                controller: 'MainCtrl'
             })
             .otherwise({
                 redirectTo: '/'
             });
     });
-
-// var mytodoApp = angular.module('mytodoApp')
-
-// function randomString(length, chars) {
-//     var result = '';
-//     for (var i = length; i > 0; --i) result += chars[Math.round(Math.random() * (chars.length - 1))];
-//     return result;
-// }
-
-// mytodoApp.factory("mytodoApp", function($http) {
-//     return {
-//         "retrieveYelp": function(name, callback) {
-//             $http.jsonp("http://api.yelp.com/v2/search?term=food&location=San+Francisco&callback=JSON_CALLBACK", {
-//                 params: {
-//                     oauth_consumer_key: "V-fz717e6id9rjQojd65HQ",
-//                     oauth_token: "6oYKZgIYiMXKSgofWWYJDuhiukvX3JVk",
-//                     oauth_signature_method: "hmac-sha1",
-//                     oauth_signature: "xZCZ4a3vcp02VTvHlVXw5Kkb__k",
-//                     oauth_timestamp: new Date().getTime(),
-//                     oauth_nonce: randomString(32, '0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ')
-//                 }
-//             }).success(callback);
-
-//         }
-//     }
-// });
--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -1,62 +1,9 @@
 'use strict';
 
-// angular.module('mytodoApp')
-//   .controller('MainCtrl', function ($scope, localStorageService) {
-
-//     var todosInStore = localStorageService.get('todos');
-
-//     $scope.todos = todosInStore && todosInStore.split('\n') || [];
-
-//     $scope.$watch('todos', function () {
-//       localStorageService.add('todos', $scope.todos.join('\n'));
-//     }, true);
-
-//     $scope.addTodo = function () {
-//       $scope.todos.push($scope.todo);
-//       $scope.todo = '';
-//     };
-
-//     $scope.removeTodo = function (index) {
-//       $scope.todos.splice(index, 1);
-//     };
-
-//   });
-
-angular.module('mytodoApp')
-.controller('FetchController', ['$scope', '$http', '$templateCache',
-  function($scope, $http, $templateCache) {
-    $scope.method = 'GET';
-    $scope.url = 'http://api.yelp.com/v2/search',
-
-    // "retrieveYelp": function(name, callback) {
-    //         $http.jsonp("http://api.yelp.com/v2/search?term=food&location=San+Francisco&callback=JSON_CALLBACK", {
-    //             params: {
-    //                 oauth_consumer_key: "V-fz717e6id9rjQojd65HQ",
-    //                 oauth_token: "6oYKZgIYiMXKSgofWWYJDuhiukvX3JVk",
-    //                 oauth_signature_method: "hmac-sha1",
-    //                 oauth_signature: "xZCZ4a3vcp02VTvHlVXw5Kkb__k",
-    //                 oauth_timestamp: new Date().getTime(),
-    //                 oauth_nonce: randomString(32, '0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ')
-    //             }
-    //         }).success(callback);
-
-    $scope.fetch = function() {
-      $scope.code = null;
-      $scope.response = null;
-
-      $http({method: $scope.method, url: $scope.url, cache: $templateCache}).
-        success(function(data, status) {
-          $scope.status = status;
-          $scope.data = data;
-        }).
-        error(function(data, status) {
-          $scope.data = data || "Request failed";
-          $scope.status = status;
-      });
-    };
-
-    $scope.updateModel = function(method, url) {
-      $scope.method = method;
-      $scope.url = url;
-    };
-  }]);
+angular.module('restaurantApp')
+  .controller('MainCtrl', function ($http, $scope) {
+    $http.get('http://api.yelp.com/v2/search')
+       .then(function(res){
+          $scope.todos = res.data;                
+        });
+  });
